d71c3d8319647d7816ade4fd06e1524b34d8438b
Sammy Javed <sammyjaved@gmail.com>
2017-06-01 00:21:17 -0700

    server.py is now functional. need to integrate it w/ the main app

--- app/server.py
+++ app/server.py
@@ -6,7 +6,7 @@ import BaseHTTPServer
 # HOST_NAME = 'example.net' # !!!REMEMBER TO CHANGE THIS!!!
 HOST_NAME = 'localhost'
 PORT_NUMBER = 8088
-
+url = "Problem.mp3"
 
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_HEAD(s):
@@ -15,15 +15,25 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         s.end_headers()
     def do_GET(s):
         """Respond to a GET request."""
+        if (s.path == "/" + str(url)):
+            print("same url")
+            s.send_response(200)
+            s.send_header("Content-type", "multipart/form-data")
+            s.end_headers()
+            file = open(url, 'rb')
+            l = file.read(1024)
+            while(l):
+                s.wfile.write(l)
+                l = file.read(1024)
+            file.close()
+            return
         s.send_response(200)
         s.send_header("Content-type", "text/html")
         s.end_headers()
-        s.wfile.write("<html><head><title>Title goes here.</title></head>")
-        s.wfile.write("<body><p>This is a test.</p>")
-        # If someone went to "http://something.somewhere.net/foo/bar/",
-        # then s.path equals "/foo/bar/".
-        s.wfile.write("<p>You accessed path: %s</p>" % s.path)
-        s.wfile.write("</body></html>")
+        s.wfile.write("Download <a href=")
+        s.wfile.write("%s" % url)
+        s.wfile.write(">file</a>")
+
 
 if __name__ == '__main__':
     server_class = BaseHTTPServer.HTTPServer
