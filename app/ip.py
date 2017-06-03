a2b41a8c067f606e1858a589cd0efe3f582a510e
Sammy Javed <sammyjaved@gmail.com>
2017-06-03 02:52:41 -0700

    making server multithreaded

---
+++ app/ip.py
@@ -0,0 +1,3 @@
+import socket
+def get_ip_addr():
+	return ([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('192.168.2.1', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
